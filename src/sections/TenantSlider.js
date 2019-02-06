8b4eded8f780a4fd42bd7d3ccd18e7adb061cb0f
Geoff Cronin <geoff.cronin@imaginuity.com>
2019-02-06 09:54:55 -0600

    Added button

--- src/sections/TenantSlider.js
+++ src/sections/TenantSlider.js
@@ -1,6 +1,8 @@
 import React from "react";
+import Link from 'react-static';
 import Slider from "react-slick";
 import ReactHtmlParser from 'react-html-parser';
+import Button from 'reactstrap';
 
 var storeArray = [];
 
@@ -14,7 +16,7 @@ export default class TenantSlider extends React.Component {
         const stores = this.props.stores;
         storeArray = stores.map(store => {
             if (store.acf.featured_image != null && store.acf.featured_image.length > 0){
-                return <div><img key={store.acf.featured_image} src={store.acf.featured_image} /><h4 key={store.slug}>{ReactHtmlParser(store.title.rendered)}</h4><div>{ReactHtmlParser(store.acf.store_copy)}</div></div>
+                return <div><img key={store.acf.featured_image} src={store.acf.featured_image} /><h4 key={store.slug}>{ReactHtmlParser(store.title.rendered)}</h4><div>{ReactHtmlParser(store.acf.store_copy)}</div><a href={`/shopping/${store.slug}/`}>Learn More</a></div>
             } else {
                 return (null)
             }
@@ -29,7 +31,6 @@ export default class TenantSlider extends React.Component {
         slidesToShow: 1,
         slidesToScroll: 1
         };
-
 
     return (
         <div id="tenantSlider">
