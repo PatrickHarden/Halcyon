47a676ce5470d55f2309b0504a2e369414f2d71b
Geoff Cronin <geoff.cronin@imaginuity.com>
2019-02-11 17:04:53 -0600

    fuu

--- src/singles/Post.js
+++ src/singles/Post.js
@@ -9,19 +9,25 @@ import ReactHtmlParser from 'react-html-parser'
 
 export default withRouteData(({ post }) => (
   <section>
-    <Head>
-      <body className={'single-blog blog-id-'+post.id + ' ' + post.slug} />
-      {/* {(post.yoast_meta.yoast_wpseo_title) ? <title>{post.yoast_meta.yoast_wpseo_title}</title> : <title>{title}</title>}
-      {(post.yoast_meta.yoast_wpseo_metadesc) ? <meta name="description" content={post.yoast_meta.yoast_wpseo_metadesc} /> : <meta name="description" content={metaDescription} />}
-      {(post.yoast_meta.yoast_wpseo_canonical) ? <link rel="canonical" href={post.yoast_meta.yoast_wpseo_canonical} /> : <link rel="canonical" href={siteRoot} /> } */}
-    </Head>
-    <Container>
-      <Row>
-        <Col xs="12">
-          <h1>{(post.title.rendered)? <div>{ReactHtmlParser(post.title.rendered)}</div>: ""}</h1>
-          {(post.content.rendered) ? <div>{ReactHtmlParser(post.content.rendered)}</div>: ""}
-        </Col>
-      </Row>
-    </Container>
+    {
+      typeof window !== undefined && ( 
+        <div>
+          <Head>
+        <body className={'single-blog blog-id-'+post.id + ' ' + post.slug} />
+        {/* {(post.yoast_meta.yoast_wpseo_title) ? <title>{post.yoast_meta.yoast_wpseo_title}</title> : <title>{title}</title>}
+        {(post.yoast_meta.yoast_wpseo_metadesc) ? <meta name="description" content={post.yoast_meta.yoast_wpseo_metadesc} /> : <meta name="description" content={metaDescription} />}
+        {(post.yoast_meta.yoast_wpseo_canonical) ? <link rel="canonical" href={post.yoast_meta.yoast_wpseo_canonical} /> : <link rel="canonical" href={siteRoot} /> } */}
+      </Head>
+      <Container>
+        <Row>
+          <Col xs="12">
+            <h1>{(post.title.rendered)? <div>{ReactHtmlParser(post.title.rendered)}</div>: ""}</h1>
+            {(post.content.rendered) ? <div>{ReactHtmlParser(post.content.rendered)}</div>: ""}
+          </Col>
+        </Row>
+      </Container>
+      </div>
+      ) 
+    }
   </section>
 ))
