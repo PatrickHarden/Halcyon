87698b4dc2726f768640eadc1574da510d544e14
Geoff Cronin <geoff.cronin@imaginuity.com>
2019-02-11 11:12:39 -0600

    seo updates

--- static.config.js
+++ static.config.js
@@ -22,7 +22,6 @@ export default {
       footerMenu
     }
   },
-  
   getRoutes: async () => {
     const baseURL = 'https://halcyon.dev.v3.imaginuitycenters.com'
     const { data: pages } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/pages?per_page=99')
