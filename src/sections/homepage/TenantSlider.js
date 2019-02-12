845eb9987193208e54299a2c89c85c21a4d829db
geoff.cronin <geoff.cronin@imaginuity.com>
2019-02-12 17:04:16 -0600

    space

--- src/sections/homepage/TenantSlider.js
+++ src/sections/homepage/TenantSlider.js
@@ -94,6 +94,7 @@ export default class TenantSlider extends React.Component {
 
     componentWillMount(){
 
+
         const stores = this.props.stores;
         const selectedStores = this.props.selectedStores;
 
