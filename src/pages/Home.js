8a35d8de77ad86ac42acf3b2b5705372ef28a99f
geoff.cronin <geoff.cronin@imaginuity.com>
2019-02-14 15:35:24 -0600

    home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -96,8 +96,6 @@ export default withRouteData(class Home extends React.Component {
         <article id="home">
           <Head>
             <body className="home" />
-            <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
-            <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
             {(home.yoast_meta.yoast_wpseo_title) ? <title>{home.yoast_meta.yoast_wpseo_title}</title> : ""}
             {(home.yoast_meta.yoast_wpseo_metadesc) ? <meta name="description" content={home.yoast_meta.yoast_wpseo_metadesc} /> : ""}
             {(home.yoast_meta.yoast_wpseo_canonical) ? <link rel="canonical" href={home.yoast_meta.yoast_wpseo_canonical} /> : "" }
