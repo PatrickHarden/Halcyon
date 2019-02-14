1884bafed50745f972b27b5e49b17239b0ea35a0
Geoff Cronin <geoff.cronin@imaginuity.com>
2019-02-14 15:34:31 -0600

    added modules to homepage, remove homepage modules, added color class.

--- src/singles/Page.js
+++ src/singles/Page.js
@@ -2,23 +2,15 @@
 import React from 'react'
 import { withRouteData, Link, Head } from 'react-static'
 import { Container, Row, Col } from 'reactstrap'
-import Slider from "react-slick";
 import '../css/components/pageContent.css'
 import ImageCarousel from '../sections/modules/ImageCarousel.js'
 import GlobalImageGrid from '../sections/modules/GlobalImageGrid.js'
 import FeaturedEvents from '../sections/modules/FeaturedEvents.js'
 import ContentArea from '../sections/modules/ContentArea.js'
+import FeaturedStores from '../sections/modules/FeaturedStores.js'
 
 import ReactHtmlParser from 'react-html-parser'
 
-var settings = {
-  dots: false,
-  infinite: true,
-  speed: 500,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  arrows: false,
-};
 
 export default withRouteData(class Page extends React.Component {
 
@@ -28,9 +20,27 @@ export default withRouteData(class Page extends React.Component {
 
   convertLink(url){
     var words = url.split('/');
-    console.log(words)
     if (words[4] == ""){
-      
+      return words[3]
+    } else {
+      if (words[3] == "events") {
+        return "/events/" + words[4]
+      } else if (words[3] == "sales"){
+        return "/sales/" + words[4]
+      }  else if (words[3] == "stores"){
+        return "/stores/" + words[4]
+      } else if (words[3] == "blog"){
+        return "/blogs/" + words[4]
+      }
+    }
+  }
+
+  getTitleFromUrl(url){
+    var words = url.split('/');
+    if (words[4] == ""){
+      return words[3].replace(/-/g, ' ')
+    } else {
+      return words[4].replace(/-/g, ' ')
     }
   }
 
@@ -43,16 +53,31 @@ export default withRouteData(class Page extends React.Component {
     return(
       <section>
         <Head>
-          <body className={'single-page page-id-'+page.id + ' ' + page.slug} />
+          <body className={'single-page page-id-'+page.id + ' ' + page.slug + ' ' + page.acf.global_page_color} />
           {(page.yoast_meta.yoast_wpseo_title) ? <title>{page.yoast_meta.yoast_wpseo_title}</title> : <title>{title}</title>}
           {(page.yoast_meta.yoast_wpseo_metadesc) ? <meta name="description" content={page.yoast_meta.yoast_wpseo_metadesc} /> : <meta name="description" content={metaDescription} />}
           {(page.yoast_meta.yoast_wpseo_canonical) ? <link rel="canonical" href={page.yoast_meta.yoast_wpseo_canonical} /> : <link rel="canonical" href={siteRoot} /> }
           {(page.acf.layout) ? <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" /> : ""}
         </Head>
+        <div id="heroSection">
+          <img className="hidden-xs" src={page.acf.desktop_image.url} alt={page.acf.desktop_image.alt} />
+          <img className="visible-xs" src={page.acf.mobile_image.url} alt={page.acf.mobile_image.alt} />
+          <h1>{(page.acf.title_h1) ? <div>{page.acf.title_h1}</div> : <div>{page.title.rendered}</div>}</h1>
+        </div>
           {(page.acf.additional_content) ? <Container>
             {(page.acf.subheading) ? <h2>{ReactHtmlParser(page.acf.subheading)}</h2> : ""}
             {(page.acf.content) ? <div>{ReactHtmlParser(page.acf.content)}</div> : ""}
-            {(page.acf.button_1) ? <Link className="halcyon-button" to={this.convertLink(page.acf.button_1.url)} target={page.acf.button_1.url}>{ReactHtmlParser(page.acf.button_1.title)}</Link> : ""}
+            <Row>
+              <Col sm={4}>
+                {(page.acf.button_1) ? <Link className="halcyon-button" to={this.convertLink(page.acf.button_1.url)}>{ReactHtmlParser(page.acf.button_1.title)}</Link> : ""}           
+              </Col>
+              <Col sm={4}>
+                {(page.acf.button_2) ? <Link className="halcyon-button" to={this.convertLink(page.acf.button_2.url)}>{(page.acf.button_2.title) ? <div>{ReactHtmlParser(page.acf.button_2.title)}</div>: <div>{this.getTitleFromUrl(page.acf.button_2.url)}</div>}</Link> : ""}              
+              </Col>
+              <Col sm={4}>
+                {(page.acf.button_3) ? <Link className="halcyon-button" to={this.convertLink(page.acf.button_3.url)}>{(page.acf.button_3.title) ? <div>{ReactHtmlParser(page.acf.button_3.title)}</div>: <div>{this.getTitleFromUrl(page.acf.button_3.url)}</div>}</Link> : ""}        
+              </Col>
+            </Row>
           </Container> : ""}
         {(page.acf.layout) ? 
         <div>
@@ -66,7 +91,7 @@ export default withRouteData(class Page extends React.Component {
             } else if (section.acf_fc_layout == 'featured_events') {
               return <Container key={index}><FeaturedEvents section={section} /></Container>
             } else if (section.acf_fc_layout == 'featured_stores') {
-              return <Container key={index}>featured stores</Container>
+              return <Container key={index}><FeaturedStores pageData={page} section={section} /></Container>
             }
           })}
         </div> : 
