944ec038b9d4f52f4bdbc8af890ce16334663c93
geoff.cronin <geoff.cronin@imaginuity.com>
2019-02-18 11:22:03 -0600

    nav icon and content with Featured Image

--- static.config.js
+++ static.config.js
@@ -27,7 +27,8 @@ export default {
       events,
       stores
     }
-  },  
+  },
+    
   getRoutes: async () => {
     const baseURL = 'https://halcyon.dev.v3.imaginuitycenters.com'
     const { data: pages } = await axios.get(baseURL + '/index.php/wp-json/wp/v2/pages?per_page=99')
