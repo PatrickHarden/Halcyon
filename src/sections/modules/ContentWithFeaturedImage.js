bb17cd126e89496957abb0fcf277dc68374793a3
Geoff Cronin <geoff.cronin@imaginuity.com>
2019-02-18 14:16:23 -0600

    got gform data to post

--- src/sections/modules/ContentWithFeaturedImage.js
+++ src/sections/modules/ContentWithFeaturedImage.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from 'react-static'
 import { Container, Row, Col } from 'reactstrap';
 import ReactHtmlParser from 'react-html-parser';
 import '../../css/modules/contentWithFeaturedImage.css';
@@ -9,9 +10,36 @@ export default class ContentWithFeaturedImage extends React.Component {
         super(props);
     }
 
+    convertLink(url){
+        var words = url.split('/');
+        if (words[4] == ""){
+          return words[3]
+        } else {
+          if (words[3] == "events") {
+            return "/events/" + words[4]
+          } else if (words[3] == "sales"){
+            return "/sales/" + words[4]
+          }  else if (words[3] == "stores"){
+            return "/stores/" + words[4]
+          } else if (words[3] == "blog"){
+            return "/blogs/" + words[4]
+          }
+        }
+      }
+    
+      getTitleFromUrl(url){
+        var words = url.split('/');
+        if (words[4] == ""){
+          return words[3].replace(/-/g, ' ')
+        } else {
+          return words[4].replace(/-/g, ' ')
+        }
+      }
+
     render() {
 
     return (
+        
         <Container className='contentWithFeaturedImage'>
             <Row className={(this.props.section.display_options == 'content-left-image-right') ? 'content-left' : 'content-right'}>
                 <Col sm={6} className='image-column'>
@@ -24,9 +52,7 @@ export default class ContentWithFeaturedImage extends React.Component {
                     {this.props.section.content &&
                         <div>{ReactHtmlParser(this.props.section.content)}</div>
                     }
-                    {this.props.section.button &&
-                        <a className='halcyon-button' href={this.props.section.button.url} target={this.props.section.button.target}>{this.props.section.button.title}</a>
-                    }
+                    {(this.props.section.button) ? <Link className="halcyon-button" to={this.convertLink(this.props.section.button.url)}>{(this.props.section.button.title) ? <div>{ReactHtmlParser(this.props.section.button.title)}</div>: <div>{this.getTitleFromUrl(this.props.section.button.url)}</div>}</Link> : ""} 
                 </Col>
             </Row> 
         </Container>
