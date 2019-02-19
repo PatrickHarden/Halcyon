24be9f0cf83f823c341464d95af73e0c675ae5f4
Geoff Cronin <geoff.cronin@imaginuity.com>
2019-02-19 17:31:37 -0600

    added header to content with featured image

--- src/sections/modules/ContentWithFeaturedImage.js
+++ src/sections/modules/ContentWithFeaturedImage.js
@@ -47,7 +47,14 @@ export default withSiteData(class ContentWithFeaturedImage extends React.Compone
 
     render() {
 
-    return (   
+    return (<div>
+        {this.props.section.heading &&
+        <div className='heading-container'>
+            <Container>
+            <h2>{this.props.section.heading}</h2>
+            </Container>
+        </div>
+        }     
         <Container className='contentWithFeaturedImage'>
             <Row className={(this.props.section.display_options == 'content-left-image-right') ? 'content-left' : 'content-right'}>
                 <Col sm={6} lg={7} className='image-column'>
@@ -64,6 +71,7 @@ export default withSiteData(class ContentWithFeaturedImage extends React.Compone
                 </Col>
             </Row> 
         </Container>
+        </div>
     );
   }
 })
