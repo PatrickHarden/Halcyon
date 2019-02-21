3d5f26e9a693268717baf8ab4a8fcada49002c75
Geoff Cronin <geoff.cronin@imaginuity.com>
2019-02-21 15:09:01 -0600

    Added store hours

--- src/sections/modules/ShoppingDirectory.js
+++ src/sections/modules/ShoppingDirectory.js
@@ -128,21 +128,18 @@ export default withSiteData(class ShoppingDirectory extends React.Component {
                 globalHolidayHours.push(this.props.centerInfo.acf.alternate_hours[0])
             }
         }
-        console.log(globalHolidayHours)
     }
 
     getHours(store){
         // If store holiday hours are present and today
-        console.log(store)
         var customStoreHolidayHours = false
         var customStoreHours = false
         var holidayHours = false
-        var storeHours = false
         if (store.acf.alternate_hours.length != 0){
             var hourArray = store.acf.alternate_hours
             for (var i = 0; i < hourArray.length; i++){
                 var testDate = new Date(store.acf.alternate_hours[i].date_picker)
-                if(testDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
+                if(testDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0) && store.acf.alternate_hours[i].closed == true) {
                     customStoreHolidayHours = true
                     return store.acf.alternate_hours[i].start_time + " - " + store.acf.alternate_hours[i].end_time
                 }
@@ -150,55 +147,124 @@ export default withSiteData(class ShoppingDirectory extends React.Component {
         }
         if (store.acf.custom_hours == true && customStoreHolidayHours == false){
             if (days[day] == "Monday"){
-                if (store.acf.standard_hours[0].thursday_closed == false){
+                if (store.acf.standard_hours[0].monday_closed == false){
                     customStoreHours = true;
-                    return store.acf.standard_hours[0].thursday_open + " - " + store.acf.standard_hours[0].thursday_close
+                    return store.acf.standard_hours[0].monday_open + " - " + store.acf.standard_hours[0].monday_close
+                } else {
+                    return 'Closed'
                 }
             } else if (days[day] == "Tuesday"){
-                if (store.acf.standard_hours[0].thursday_closed == false){
+                if (store.acf.standard_hours[0].tuesday_closed == false){
                     customStoreHours = true;
-                    return store.acf.standard_hours[0].thursday_open + " - " + store.acf.standard_hours[0].thursday_close
+                    return store.acf.standard_hours[0].tuesday_open + " - " + store.acf.standard_hours[0].tuesday_close
+                } else {
+                    return 'Closed'
                 }
             } else if (days[day] == "Wednesday"){
-                if (store.acf.standard_hours[0].thursday_closed == false){
+                if (store.acf.standard_hours[0].wednesday_closed == false){
                     customStoreHours = true;
-                    return store.acf.standard_hours[0].thursday_open + " - " + store.acf.standard_hours[0].thursday_close
+                    return store.acf.standard_hours[0].wednesday_open + " - " + store.acf.standard_hours[0].wednesday_close
+                } else {
+                    return 'Closed'
                 }
             } else if (days[day] == "Thursday"){
                 if (store.acf.standard_hours[0].thursday_closed == false){
                     customStoreHours = true;
                     return store.acf.standard_hours[0].thursday_open + " - " + store.acf.standard_hours[0].thursday_close
+                } else {
+                    return 'Closed'
                 }
             } else if (days[day] == "Friday"){
-                if (store.acf.standard_hours[0].thursday_closed == false){
+                if (store.acf.standard_hours[0].friday_closed == false){
                     customStoreHours = true;
-                    return store.acf.standard_hours[0].thursday_open + " - " + store.acf.standard_hours[0].thursday_close
+                    return store.acf.standard_hours[0].friday_open + " - " + store.acf.standard_hours[0].friday_close
+                } else {
+                    return 'Closed'
                 }
             } else if (days[day] == "Saturday"){
-                if (store.acf.standard_hours[0].thursday_closed == false){
+                if (store.acf.standard_hours[0].saturday_closed == false){
                     customStoreHours = true;
-                    return store.acf.standard_hours[0].thursday_open + " - " + store.acf.standard_hours[0].thursday_close
+                    return store.acf.standard_hours[0].saturday_open + " - " + store.acf.standard_hours[0].saturday_close
+                } else {
+                    return 'Closed'
                 }
             } else if (days[day] == "Sunday"){
-                if (store.acf.standard_hours[0].thursday_closed == false){
+                if (store.acf.standard_hours[0].sunday_closed == false){
                     customStoreHours = true;
-                    return store.acf.standard_hours[0].thursday_open + " - " + store.acf.standard_hours[0].thursday_close
+                    return store.acf.standard_hours[0].sunday_open + " - " + store.acf.standard_hours[0].sunday_close
+                } else {
+                    return 'Closed'
                 }
             }
         }
-        if (store.acf.custom_hours == false && customStoreHolidayHours == false && customStoreHours == false){
-            console.log('etset')
-            if (globalHolidayHours != 0){
-                var hourArray = store.acf.alternate_hours
+        if (store.acf.custom_hours == false && customStoreHolidayHours == false){
+            if (globalHolidayHours.length != 0){
+                var hourArray = globalHolidayHours
                 for (var i = 0; i < hourArray.length; i++){
-                    var testDate = new Date(store.acf.alternate_hours[i].date_picker)
-                    if(testDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0)) {
-                        customStoreHolidayHours = true
-                        return store.acf.alternate_hours[i].start_time + " - " + store.acf.alternate_hours[i].end_time
+                    var testDate = new Date(globalHolidayHours[i].date_picker)
+                    if(testDate.setHours(0,0,0,0) == todaysDate.setHours(0,0,0,0) && globalHolidayHours[i].closed == true) {
+                        console.log('holiday hours true')
+                        holidayHours = true
+                        return globalHolidayHours[i].start_time + " - " + globalHolidayHours[i].end_time
                     }
                 }
             }
         }
+        if (store.acf.custom_hours == false && customStoreHolidayHours == false && holidayHours == false){
+            if (globalHours.length != 0){
+                console.log(globalHours, 'global hours')
+                if (days[day] == "Monday"){
+                    if (globalHours[0].monday_closed == false){
+                        customStoreHours = true;
+                        return globalHours[0].monday_open + " - " + globalHours[0].monday_close
+                    } else {
+                        return 'Closed'
+                    }
+                } else if (days[day] == "Tuesday"){
+                    if (globalHours[0].tuesday_closed == false){
+                        customStoreHours = true;
+                        return globalHours[0].tuesday_open + " - " + globalHours[0].tuesday_close
+                    } else {
+                        return 'Closed'
+                    }
+                } else if (days[day] == "Wednesday"){
+                    if (globalHours[0].wednesday_closed == false){
+                        customStoreHours = true;
+                        return globalHours[0].wednesday_open + " - " + globalHours[0].wednesday_close
+                    } else {
+                        return 'Closed'
+                    }
+                } else if (days[day] == "Thursday"){
+                    if (globalHours[0].thursday_closed == false){
+                        customStoreHours = true;
+                        return globalHours[0].thursday_open + " - " + globalHours[0].thursday_close
+                    } else {
+                        return 'Closed'
+                    }
+                } else if (days[day] == "Friday"){
+                    if (globalHours[0].friday_closed == false){
+                        customStoreHours = true;
+                        return globalHours[0].friday_open + " - " + globalHours[0].friday_close
+                    } else {
+                        return 'Closed'
+                    }
+                } else if (days[day] == "Saturday"){
+                    if (globalHours[0].saturday_closed == false){
+                        customStoreHours = true;
+                        return globalHours[0].saturday_open + " - " + globalHours[0].saturday_close
+                    } else {
+                        return 'Closed'
+                    }
+                } else if (days[day] == "Sunday"){
+                    if (globalHours[0].sunday_closed == false){
+                        customStoreHours = true;
+                        return globalHours[0].sunday_open + " - " + globalHours[0].sunday_close
+                    } else {
+                        return 'Closed'
+                    }
+                } 
+            }
+        }    
     }
 
 
