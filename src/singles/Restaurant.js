791c054e2d830cd0376fafc97b4a2f8a368e8d65
Geoff Cronin <geoff.cronin@imaginuity.com>
2019-02-22 16:04:29 -0600

    updated restaurant and retailer page

--- src/singles/Restaurant.js
+++ src/singles/Restaurant.js
@@ -5,6 +5,9 @@ import { Container,
          Col,} from 'reactstrap';
 
 import ReactHtmlParser from 'react-html-parser'
+import '../css/components/pageContent.css'
+import '../css/components/retailerContent.css'
+import '../css/components/mobileFloatingNav.css'
 //
 
 export default withRouteData(({ restaurant, siteRoot, title, metaDescription }) => (
@@ -15,13 +18,34 @@ export default withRouteData(({ restaurant, siteRoot, title, metaDescription }) 
       {(restaurant.yoast_meta.yoast_wpseo_metadesc) ? <meta name="description" content={restaurant.yoast_meta.yoast_wpseo_metadesc} /> : <meta name="description" content={metaDescription} />}
       {(restaurant.yoast_meta.yoast_wpseo_canonical) ? <link rel="canonical" href={restaurant.yoast_meta.yoast_wpseo_canonical} /> : <link rel="canonical" href={siteRoot} /> }
     </Head>
-    <Container>
+    <div>
+    {console.log(restaurant)}
+    <div id="heroSection">
+      {(restaurant.acf.hero_image_desktop) ? <img className="hidden-xs" src={restaurant.acf.hero_image_desktop.url} alt={restaurant.acf.hero_image_desktop.alt} /> : ""}
+      {(restaurant.acf.hero_image_mobile)? <img className="visible-xs" src={restaurant.acf.hero_image_mobile.url} alt={restaurant.acf.hero_image_mobile.alt} />  : ""}
+      <div className="store-container">
+        {(restaurant.acf.featured_image) ? <img className="featuredImage" src={restaurant.acf.featured_image} /> : "" }
+        <div className="featuredContent">
+          {(restaurant.acf.subheading)? <h5>{ReactHtmlParser(restaurant.acf.subheading)}</h5> : ""}
+          {(restaurant.acf.blurb) ? <p>{ReactHtmlParser(restaurant.acf.blurb)}</p> : "" }
+          {(restaurant.acf.street_address)? <a href={'//maps.google.com/?q=' + restaurant.acf.street_address} target="_blank">Location</a>: ""}
+          {(restaurant.acf.website)? <a href={restaurant.acf.website} target="_blank">website</a>: ""}
+          {(restaurant.acf.phone)? <a href={restaurant.acf.phone} target="_blank">phone</a>: ""}
+          {(restaurant.acf.facebook)? <a href={restaurant.acf.facebook} target="_blank">facebook</a>: ""}
+          {(restaurant.acf.instagram)? <a href={restaurant.acf.instagram} target="_blank">instagram</a>: ""}
+          {(restaurant.acf.twitter)? <a href={restaurant.acf.twitter} target="_blank">twitter</a>: ""}
+        </div>
+    </div>  
+    </div>
+    </div>
+    <div class="container text-center">
       <Row>
         <Col xs="12">
-          <h1>{restaurant.title.rendered}</h1>
-          {ReactHtmlParser(restaurant.acf.post_copy)}
+          {(restaurant.title.rendered)? <h1>{restaurant.title.rendered}</h1> : ""}
+          <p>1234 Magnolia Ave, Forsyth, GA 31029  |  Hours:  M-F 8:00am - 9pm, Sat & Sun 11am - 6pm  |  (972)000-1234  |  www.ninandleigh.com</p>
+          {ReactHtmlParser(restaurant.acf.store_copy)}
         </Col>
       </Row>
-    </Container>
+    </div>
   </section>
 ))
--- src/singles/Retailer.js
+++ src/singles/Retailer.js
@@ -5,6 +5,9 @@ import { Container,
          Col,} from 'reactstrap';
 
 import ReactHtmlParser from 'react-html-parser'
+import '../css/components/pageContent.css'
+import '../css/components/retailerContent.css'
+import '../css/components/mobileFloatingNav.css'
 //
 
 export default withRouteData(({ retailer, siteRoot, title, metaDescription }) => (
@@ -15,13 +18,34 @@ export default withRouteData(({ retailer, siteRoot, title, metaDescription }) =>
       {(retailer.yoast_meta.yoast_wpseo_metadesc) ? <meta name="description" content={retailer.yoast_meta.yoast_wpseo_metadesc} /> : <meta name="description" content={metaDescription} />}
       {(retailer.yoast_meta.yoast_wpseo_canonical) ? <link rel="canonical" href={retailer.yoast_meta.yoast_wpseo_canonical} /> : <link rel="canonical" href={siteRoot} /> }
     </Head>
-    <Container>
+    <div>
+    {console.log(retailer)}
+    <div id="heroSection">
+      {(retailer.acf.hero_image_desktop) ? <img className="hidden-xs" src={retailer.acf.hero_image_desktop.url} alt={retailer.acf.hero_image_desktop.alt} /> : ""}
+      {(retailer.acf.hero_image_mobile)? <img className="visible-xs" src={retailer.acf.hero_image_mobile.url} alt={retailer.acf.hero_image_mobile.alt} />  : ""}
+      <div className="store-container">
+        {(retailer.acf.featured_image) ? <img className="featuredImage" src={retailer.acf.featured_image} /> : "" }
+        <div className="featuredContent">
+          {(retailer.acf.subheading)? <h5>{ReactHtmlParser(retailer.acf.subheading)}</h5> : ""}
+          {(retailer.acf.blurb) ? <p>{ReactHtmlParser(retailer.acf.blurb)}</p> : "" }
+          {(retailer.acf.street_address)? <a href={'//maps.google.com/?q=' + retailer.acf.street_address} target="_blank">Location</a>: ""}
+          {(retailer.acf.website)? <a href={retailer.acf.website} target="_blank">website</a>: ""}
+          {(retailer.acf.phone)? <a href={retailer.acf.phone} target="_blank">phone</a>: ""}
+          {(retailer.acf.facebook)? <a href={retailer.acf.facebook} target="_blank">facebook</a>: ""}
+          {(retailer.acf.instagram)? <a href={retailer.acf.instagram} target="_blank">instagram</a>: ""}
+          {(retailer.acf.twitter)? <a href={retailer.acf.twitter} target="_blank">twitter</a>: ""}
+        </div>
+    </div>  
+    </div>
+    </div>
+    <div class="container text-center">
       <Row>
         <Col xs="12">
-          <h1>{retailer.title.rendered}</h1>
-          {ReactHtmlParser(retailer.acf.post_copy)}
+          {(retailer.title.rendered)? <h1>{retailer.title.rendered}</h1> : ""}
+          <p>1234 Magnolia Ave, Forsyth, GA 31029  |  Hours:  M-F 8:00am - 9pm, Sat & Sun 11am - 6pm  |  (972)000-1234  |  www.ninandleigh.com</p>
+          {ReactHtmlParser(retailer.acf.store_copy)}
         </Col>
       </Row>
-    </Container>
+    </div>
   </section>
 ))
