0afd1b2b3a9e7186cda2b0b417f7c37f612475f3
Geoff Cronin <geoff.cronin@imaginuity.com>
2019-02-27 17:30:56 -0600

    debug

--- src/sections/modules/SalesDirectory.js
+++ src/sections/modules/SalesDirectory.js
@@ -7,8 +7,7 @@ import helpers from '../../helpers.js'
 import { faPlus } from '@fortawesome/free-solid-svg-icons'
 import '../../css/modules/diningDirectory.css'
 let moment = require('moment');
-var globalHours = [];
-var globalHolidayHours = [];
+
 var saleCounter;
 var sales = [];
 
@@ -17,7 +16,7 @@ export default withSiteData(class SaleDirectory extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            amount: 4
+            amount: 6
         }
         this.loadMore = this.loadMore.bind(this);
     }
@@ -28,7 +27,7 @@ export default withSiteData(class SaleDirectory extends React.Component {
         })
     }
 
-    componentWillMount() {
+    componentDidMount() {
         saleCounter = this.props.sales.map(store => {
             if (store.acf.store_type == "restaurant") {
                 return <div></div>
@@ -40,9 +39,9 @@ export default withSiteData(class SaleDirectory extends React.Component {
 
         const siteRoot = 'https://halycon.netlify.com';
         // If sales array is full, do nothing, otherwise build out sales arraay.
-        if (sales.length != 0) {
+        if (sales) {
         } else {
-          sales = this.props.sales.slice(0, this.state.amount).map((sale, index) => {
+          sales = this.props.sales.map((sale, index) => {
             return (<div key={index} className="sale-single row">
               <div className="col-sm-2">{(sale.acf.end_date) ? <span>Ends<br />{moment(sale.acf.end_date, 'YYYYMMDD').format('MM/DD')}</span> : ""}</div>
               <div className='image-wrapper col-sm-3'>
@@ -68,13 +67,38 @@ export default withSiteData(class SaleDirectory extends React.Component {
             </div>)
           })
         }
-
     }
 
     componentWillUpdate(){
         if (saleCounter.length <= this.state.amount){
             document.getElementById('loadMore').style.display = 'none';
         }
+        sales = this.props.sales.slice(0, this.state.amount).map((sale, index) => {
+            return (<div key={index} className="sale-single row">
+              <div className="col-sm-2">{(sale.acf.end_date) ? <span>Ends<br />{moment(sale.acf.end_date, 'YYYYMMDD').format('MM/DD')}</span> : ""}</div>
+              <div className='image-wrapper col-sm-3'>
+                <img className='hidden-xs' src={sale.acf.featured_image} />
+              </div>
+              <div className="col-sm-5">
+                <div>{moment(sale.acf.start_date, 'YYYYMMDD').format('MMM DD')} - {moment(sale.acf.end_date, 'YYYYMMDD').format('MMM DD')}</div>
+                <h5>{sale.title.rendered}</h5>
+                {(sale.acf.post_copy) ? <div>{ReactHtmlParser(sale.acf.post_copy)}</div> : ""}
+              </div>
+              <div className="col-sm-2">
+                {/* <a href={'mailto:?body=' + siteRoot + '/dining/' + this.props.retailer.slug + '&subject=' + ReactHtmlParser(sale.title.rendered)}>
+                  mail
+                          </a>
+                <a href={'https://twitter.com/home?status=' + siteRoot + '/dining/' + this.props.retailer.slug} target="_blank">
+                  twitter
+                          </a>
+                <a href={'https://www.facebook.com/sharer/sharer.php?u=' + siteRoot + '/dining/' + this.props.retailer.slug} target="_blank">
+                  facebook
+                          </a> */}
+                <Link to={'/sales/' + sale.slug} className="halcyon-button">More Info ></Link>
+              </div>
+              </div>)
+          })
+        
     }
 
     render() {
