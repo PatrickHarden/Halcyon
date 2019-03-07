7faeddc9f775947bdb3516aae0335a696757b2fe
alex.patton <alex.patton@imaginuity.com>
2019-03-07 16:35:45 -0600

    readded tint social feed, applied styles to heading above

--- src/sections/modules/SocialFeed.js
+++ src/sections/modules/SocialFeed.js
@@ -2,8 +2,10 @@ import React from "react";
 import { withSiteData } from 'react-static'
 import { Container } from 'reactstrap';
 import ReactHtmlParser from 'react-html-parser';
-import JuicerFeed from 'react-juicer-feed';
-// import TintSocialFeed from '../../sections/homepage/TintSocialFeed.js';
+// import JuicerFeed from 'react-juicer-feed';
+import TintSocialFeed from '../../sections/homepage/TintSocialFeed.js';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/fontawesome-free-brands'
 
 export default withSiteData(class SocialFeed extends React.Component {
 
@@ -12,16 +14,33 @@ export default withSiteData(class SocialFeed extends React.Component {
     }
 
     render() {
-        return ( 
+        return (
             <Container className='social-feed-container'>
+                {console.log(this.props.centerInfo)}
                 {(this.props.centerInfo.acf.data_id && typeof document != 'undefined') ? <div>
-                    {this.props.section.heading && 
-                    <h2>{ReactHtmlParser(this.props.section.heading)}</h2>}
+                    <div id="socialHeader">
+                        <div>
+                            {this.props.section.heading &&
+                                <h2>{ReactHtmlParser(this.props.section.heading)}</h2>}
+                        </div>
+                        <div>
+                            <a class="social-icon" href={this.props.centerInfo.acf.facebook_url} target="_blank">
+                                <FontAwesomeIcon icon={faFacebookF} className='search-icon' />
+                            </a>
+                            <a class="social-icon" href={this.props.centerInfo.acf.twitter_url} target="_blank">
+                                <FontAwesomeIcon icon={faTwitter} className='search-icon' />
+                            </a>
+                            <a class="social-icon" href={this.props.centerInfo.acf.instagram_url} target="_blank">
+                                <FontAwesomeIcon icon={faInstagram} className='search-icon' />
+                            </a>
+                        </div>
+                    </div>
                     <div id="thisTarget">
-                        <JuicerFeed feedId='imaginuity-ba275954-4d89-4453-8ef8-80e94361aeb4' />
+                        {/* <JuicerFeed feedId='imaginuity-ba275954-4d89-4453-8ef8-80e94361aeb4' /> */}
+                        <TintSocialFeed optionsData={this.props.centerInfo} />
                     </div>
                 </div> : ""}
             </Container>
         );
-  }
+    }
 })
