b235e898ca26d22e34960be9144a20c02a4a6bb7
alex.patton <alex.patton@imaginuity.com>
2019-03-14 10:03:59 -0500

    Adjusted fonts, removed packages.

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -25,7 +25,7 @@ export default withSiteData(class SiteHeader extends React.Component {
                         <meta charSet="utf-8" />
                         <meta property="og:title" content={this.props.title} />
                         <meta property="og:image" content={this.props.centerInfo.acf.logo} />
-                        <link rel="stylesheet" href="https://use.typekit.net/osv0bnv.css" />
+                        {/* <link rel="stylesheet" href="https://use.typekit.net/osv0bnv.css" /> */}
                         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
                         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
                         {(options.acf.favicon) ? <link rel="shortcut icon" type="image/png" href={options.acf.favicon.url}/> : ""}
