e6d4e2791124c30e4a71e082b5e26418ee8a7c89
geoff.cronin <geoff.cronin@imaginuity.com>
2019-03-19 15:13:06 -0500

    replaced the correct font... patrick??

--- src/sections/Header.js
+++ src/sections/Header.js
@@ -26,6 +26,7 @@ export default withSiteData(class SiteHeader extends React.Component {
                         {/* <link rel="stylesheet" href="https://use.typekit.net/osv0bnv.css" /> */}
                         <link rel="stylesheet" type="text/css" charset="UTF-8" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css" />
                         <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css" />
+                        <link rel="stylesheet" href="https://use.typekit.net/fve7opr.css"/>
                         {(options.acf.favicon) ? <link rel="shortcut icon" type="image/png" href={options.acf.favicon.url}/> : ""}
                     </Head>
                 </header>
