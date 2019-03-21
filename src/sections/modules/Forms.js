a8f8fbb6abcf13a2b98c929465bf90fbd448fb02
geoff.cronin <geoff.cronin@imaginuity.com>
2019-03-21 14:11:18 -0500

    form fix (I think)

--- src/sections/modules/Forms.js
+++ src/sections/modules/Forms.js
@@ -61,12 +61,12 @@ export default withSiteData(class Forms extends React.Component {
                                         {field.inputs.map(function (input) {
                                             if (!input.isHidden) {
                                                 return (
-                                                    <label for={input.id}
+                                                    <label for={input.id.replace('.','')}
                                                         className="col-xs-12 col-sm-6">
                                                         <span className="sr-only test">{input.label}</span>
                                                         <input
                                                             type="text"
-                                                            id={field.id}
+                                                            id={input.id.replace('.','')}
                                                             className={"form-control" + (field.cssClass ? ' ' + field.cssClass : '')}
                                                             placeholder={input.label + (field.isRequired ? '*' : '')}
                                                             name={input.label + input.id}
